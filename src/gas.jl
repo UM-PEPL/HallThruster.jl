6e2a3ef52943af1673746496d810b1ad50b7349e
Thomas Marks <marksta@umich.edu>
2021-08-16 13:17:23 -0400

    gas docs

--- src/gas.jl
+++ src/gas.jl
@@ -3,14 +3,14 @@
 A chemical element in the gaseous state. Container for element properties used in fluid computations.
 
 # Fields
-    name::String        Full name of gas (i.e. Xenon)
-    short_name::String  Short name/symbol (i.e. Xe for Xenon)
-    γ::Float64          Specific heat ratio / adiabatic index
-    M::Float64          Molar mass (grams/mol) or atomic mass units
-    m::Float64          Mass of atom in kg
-    cp::Float64         Specific heat at constant pressure
-    cv::Float64         Specific heat at constant volume
-    R::Float64          Gas constant
+name::String        Full name of gas (i.e. Xenon)
+short_name::String  Short name/symbol (i.e. Xe for Xenon)
+γ::Float64          Specific heat ratio / adiabatic index
+M::Float64          Molar mass (grams/mol) or atomic mass units
+m::Float64          Mass of atom in kg
+cp::Float64         Specific heat at constant pressure
+cv::Float64         Specific heat at constant volume
+R::Float64          Gas constant
 """
 struct Gas
     name::String
