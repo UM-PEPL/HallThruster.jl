9105daad90487eee5ccfb687f73d281abccb381e
Thomas Marks <marksta@umich.edu>
2021-08-16 11:53:16 -0400

    update makedocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,22 @@
 using Documenter, HallThruster
 
 push!(LOAD_PATH,"../src/")
-makedocs(sitename="HallThruster.jl Documentation")
+
+makedocs(
+    modules=[PartialFunctions],
+    authors="Thomas Marks <marksta@umich.edu> and contributors",
+    repo="https://github.com/archermarx/HallThruster.jl/blob/{commit}{path}#L{line}",
+    sitename="PartialFunctions.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://archermarx.github.io/HallThruster.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Internals" => "internals.md"
+    ],
+)
 
 deploydocs(
     repo = "github.com/archermarx/HallThruster.jl.git",
