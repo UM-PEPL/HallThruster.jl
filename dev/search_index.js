4078f7cf855065986a7ddd870a430ff04ebdaa40
Documenter.jl <documenter@juliadocs.github.io>
2021-08-16 15:29:27 +0000

    build based on bdcd8b4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"-","title":"-","text":"func(x)","category":"page"},{"location":"#HallThruster.func-Tuple{Any}","page":"-","title":"HallThruster.func","text":"func(x)\n\nReturns double the number x plus 1.\n\nfunc(1)\n\n# output\n\n3\n\n\n\n\n\n","category":"method"}]
+}
