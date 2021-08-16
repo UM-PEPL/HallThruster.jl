a3c0a4f6f22feaeb128c140045cf264c725a4297
Thomas Marks <marksta@umich.edu>
2021-08-16 13:29:12 -0400

    Fix units in gas docstring

--- src/gas.jl
+++ src/gas.jl
@@ -13,11 +13,11 @@ A chemical element in the gaseous state. Container for element properties used i
 
 `m::Float64`          Mass of atom in kg
 
-`cp::Float64`         Specific heat at constant pressure in J / kmol / K
+`cp::Float64`         Specific heat at constant pressure in J / kg / K
 
-`cv::Float64`         Specific heat at constant volume in J / kmol / K
+`cv::Float64`         Specific heat at constant volume in J / kg / K
 
-`R::Float64`          Gas constant in J / kmol / K
+`R::Float64`          Gas constant in J / kg / K
 
 """
 struct Gas
