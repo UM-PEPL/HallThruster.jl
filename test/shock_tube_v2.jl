18aefa6d6ae5fdc807e6496a4331578c3004ef69
dere4 <93271216+dere4@users.noreply.github.com>
2021-11-09 13:55:37 -0500

    got rid of old simulation.jl functions, updated them to the previoulsy v2 ones. deleted shock_tube.jl.

--- test/shock_tube_v2.jl
+++ test/shock_tube_v2.jl
@@ -1,6 +1,6 @@
 using HallThruster, Plots
 
-function shock_tube_v2(fluxfn, ncells, end_time)
+function shock_tube(fluxfn, ncells, end_time)
     ρL, uL, pL = 1.0, 0.0, 1.0
     ρR, uR, pR = 0.125, 0.0, 0.1
 
@@ -36,20 +36,20 @@ function shock_tube_v2(fluxfn, ncells, end_time)
         gas1 = fluids[1].species.element
         gas2 = fluids[2].species.element
         if z < L/2
-            n1 = ρL / gas1.m
+            ρ1 = ρL
             T1 = pL / ρL * gas1.m / HallThruster.kB
-            n2 = ρL / gas2.m
+            ρ2 = ρL
             T2 = pL / ρL * gas2.m / HallThruster.kB
         else
-            n1 = ρR / gas1.m
+            ρ1 = ρR
             T1 = pR / ρR * gas1.m / HallThruster.kB
-            n2 = ρR / gas2.m
+            ρ2 = ρR
             T2 = pR / ρR * gas2.m / HallThruster.kB
 
         end
         u1 = 0.0
         u2 = 0.0
-        U .= [n1*gas1.m, n1*u1*gas1.m, n1*gas1.m*T1*gas1.cv, n2*gas2.m, n2*u2*gas2.m, n2*gas2.m*T2*gas2.cv]
+        U .= [ρ1, ρ1*u1, ρ1*T1*gas1.cv, ρ2, ρ2*u2, ρ2*T2*gas2.cv]
         return U
     end
 
@@ -57,7 +57,7 @@ function shock_tube_v2(fluxfn, ncells, end_time)
     #simulation input, need to somehow do initial conditions now as well, do as with boundary conditions in thomas code
     sim = HallThruster.MultiFluidSimulation(grid = HallThruster.generate_grid(geometry, ncells), 
     boundary_conditions = BCs,
-    scheme = HallThruster.HyperbolicScheme(fluxfn, identity, false),
+    scheme = HallThruster.HyperbolicScheme(fluxfn, HallThruster.minmod, true),
     initial_condition = IC!, 
     source_term! = source, 
     fluids = [HallThruster.Fluid(HallThruster.Species(HallThruster.Air, 0), HallThruster.EulerEquations());
@@ -65,11 +65,11 @@ function shock_tube_v2(fluxfn, ncells, end_time)
     end_time = end_time, 
     saveat = [0, end_time]
     )
-    sol = HallThruster.run_simulation_v2(sim)
+    sol = HallThruster.run_simulation(sim)
 end
 
-function run_shock_tube_v2(ncells, time)
-    @time sol = shock_tube_v2(HallThruster.HLLE!, ncells, time)
+function run_shock_tube(ncells, time)
+    @time sol = shock_tube(HallThruster.HLLE!, ncells, time)
     zs = LinRange(0, 1, ncells+2)
     p = plot(zs, sol.u[1][1, :])
     plot!(zs, sol.u[end][4, :])
