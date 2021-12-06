9766d1dbad3093d4cfd1769176d8eb89dfd63ef2
dere4 <93271216+dere4@users.noreply.github.com>
2021-12-06 15:55:43 -0500

    Added verification for potential discretization and electron transport order is 1.99999999 with consistent interpolation on boundaries somehow this interpolation causes instabilities in the full sim dunno why, but boundaries will need to be changed anyways also weird that potential does not evolve over a simulation from t = 0 to t = 0.0002 at the moment possibly changing terms such as ne, nn, ni*ui are not changing sufficiently over time. Need to think about this.

--- test/runtests.jl
+++ test/runtests.jl
@@ -324,6 +324,24 @@ end
     @test_throws(ArgumentError, HallThruster.apply_bc!(U, BC2, :not_left_or_right))
 end
 
+@testset "Electron transport tests" begin
+    Tev = 30 #[eV]
+    m = HallThruster.Xenon.m #
+    Te = Tev*HallThruster.e/HallThruster.kB #
+    ne = 1e18 #[#/m^3]
+    nn = 0.5e18 #[#/m^3]
+    B = 1.0
+    ν_an = 0.0
+    σ_en = 6.6e-19*((Tev/4 - 0.1)/(1 + (Tev/4)^1.6)) #[m^2]
+    @test σ_en ≈ HallThruster.σ_en(Tev)
+    ln_λ = 23 - 0.5*log(1e-6*ne/Tev^3)
+    @test ln_λ ≈ HallThruster.ln_λ(ne, Tev)
+    ν_c = σ_en*nn*sqrt(8*HallThruster.kB*Te/pi/m) + 2.9e-12*ne*ln_λ/(Tev)^1.5
+    @test ν_c ≈ HallThruster.get_v_c(Tev, nn, ne, m)
+    μ_e = HallThruster.e/(HallThruster.mₑ * ν_c)/(1+(HallThruster.e*B/(HallThruster.mₑ*ν_c))^2)
+    @test μ_e ≈ HallThruster.cf_electron_transport(ν_an, ν_c, B)
+end
+
 #=
 @testset "Simulation setup tests" begin
     @test SPT_100 isa HallThruster.Geometry1D
@@ -529,6 +547,16 @@ end
     for i in 1:length(results)
         println("Simulation with $(results[i].ncells) cells and dt $(results[i].timestep[1]) converged after $(round(results[i].solution.t[1]/results[i].timestep[1])) timesteps at time $(results[i].solution.t[1])")
     end
+end
+
+@testset "Order verficication study of Potential, with analytic solution for d^2x/dy^2 = 50000, Dirichlet boundaries" begin
+    results = perform_OVS_potential(; MMS_CONSTS, fluxfn = HallThruster.HLLE!, reconstruct = false)
+    L_1, L_inf = evaluate_slope(results, MMS_CONSTS)
+    expected_slope = 2
+    @test L_1[1] ≈ expected_slope atol = expected_slope*0.1
+    println("L_1 $(L_1[1])")
+    @test L_inf[1] ≈ expected_slope atol = expected_slope*0.1
+    println("L_inf $(L_inf[1])")
 end
 
 @testset "Test ion acceleration source term" begin
