2ae44a1f67f81e7c59b760172156bf0ccfffb2a0
dere4 <93271216+dere4@users.noreply.github.com>
2021-12-20 18:11:13 -0500

    added a bunch of variables to state vector, still get very strong oscillations in energy, tried to find cause, electron velocity oscillating between +- 200000 in the first few cells. tried to find cause of that, grad phi looks good, grad epsilon oscillates but not as strong. need to check electron transport, which in turn is dependent on neutral density through v_c, which does not seems like it oscillates though.

--- test/run.jl
+++ test/run.jl
@@ -21,8 +21,7 @@ end
 function IC!(U, z, fluids, L)
     ρ2 = 2.1801715574645586e-7 #ρ1 * exp(-((z - L) / 0.033)^2)
     u1 = 150.0
-    ρ1 = 5e-6/HallThruster.Xenon.m/0.004/u1
-    @show ρ1
+    ρ1 = 5e-6/0.004/u1
     U[1] = ρ1
     U[2] = ρ2
     U .= SA[ρ1, ρ2, ρ2*u1] #[ρ1, ρ1*u1, ρ1*E]
@@ -42,8 +41,9 @@ function run_sim(end_time = 0.0002, n_save = 2)
     timestep = 0.1e-9 #0.9e-8
 
     ρ2 = 2.1801715574645586e-7 #ρ1 * exp(-((z - L) / 0.033)^2)
-    u1 = 150.0
-    ρ1 = 5e-6/HallThruster.Xenon.m/0.004/u1
+    u1 = 150.0 #150
+    ρ1 = 5e-6/0.004/u1
+    @show ρ1/HallThruster.Xenon.m
     T1 = 1000.0
 
     left_state = [ρ1, ρ2, ρ2 * u1] # [ρ1, ρ1*u1, ρ1*E]
@@ -105,19 +105,31 @@ function animate_solution(sol, saved_values)
         plot!(p, u[3, :] ./ u[2, :])
     end
     @gif for (u, t) in zip(sol.u, sol.t)
-        p = plot(ylims = (1e17, 1e21))
-        plot!(p, u[4, :])
+        p = plot(ylims = (1e13, 1e21))
+        plot!(p, u[4, :], yaxis = :log)
     end
-    @gif for (ϕ, t) in zip(ϕ, sol.t)
+    @gif for (u, t) in zip(sol.u, sol.t) #ϕ
         p = plot(ylims = (-100, 400))
-        plot!(p, ϕ)
+        plot!(p, u[5, :])
     end
-    @gif for (Tev, t) in zip(Tev, sol.t)
+    @gif for (u, t) in zip(sol.u, sol.t) #Tev
         p = plot(ylims = (0, 30))
-        plot!(p, Tev)
+        plot!(p, u[4, :] ./ 3 .* 2 ./ u[6, :])
     end
-    @gif for (ne, t) in zip(ne, sol.t)
-        p = plot(ylims = (1e13, 1e20))
-        plot!(p, ne)
+    @gif for (u, t) in zip(sol.u, sol.t) #ne
+        p = plot(ylims = (1e16, 1e20))
+        plot!(p, u[6, :], yaxis = :log)
+    end
+    @gif for (u, t) in zip(sol.u, sol.t) #uₑ
+        p = plot(ylims = (-1e6, 1e6))
+        plot!(p, u[7, :], yaxis = :log)
+    end
+    @gif for (u, t) in zip(sol.u, sol.t) #- grad_ϕ
+        p = plot(ylims = (0, 5e5))
+        plot!(p, u[8, :])
+    end
+    @gif for (u, t) in zip(sol.u, sol.t) # grad_pe/e/params.cache.ne[i]
+        p = plot(ylims = (0, 5e5))
+        plot!(p, u[7, :], yaxis = :log)
     end
 end
