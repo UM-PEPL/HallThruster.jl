7a35730fcba5c5404c3ef29e662b7f3ceba8c813
Thomas Marks <marksta@umich.edu>
2022-04-05 10:09:13 -0400

    you forgot a comma

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ makedocs(
             "boundary_conditions.md",
             "source_terms.md",
         ],
-        "Verification" => "verification.md"
+        "Verification" => "verification.md",
         "Internals" => "internals.md"
     ],
 )
