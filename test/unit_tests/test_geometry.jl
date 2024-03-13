e61e3b0cfc4cc644dabfe972cb7134ba52d7386d
Declan Brick <brickd@umich.edu>
2024-03-13 14:36:51 -0400

    Add unit tests for plume expansion calculation

--- test/unit_tests/test_geometry.jl
+++ test/unit_tests/test_geometry.jl
@@ -25,4 +25,35 @@
     @test HallThruster.channel_perimeter(r1, r0) ≈ 2π * (r1 + r0)
     @test HallThruster.channel_perimeter(HallThruster.SPT_100) ≈ 2π * (r1 + r0)
 
+
+    
+    U = zeros(2, 4) 
+    U[1, :] .= 1e17
+    U[2, :] = 1e17 .*  [-100, 100, 5000, 20000]
+
+    inner_radius = zeros(4)
+    outer_radius = zeros(4)
+    channel_height = zeros(4)
+    dA_dz = zeros(4)
+    tanδ = zeros(4)
+
+    cs = sqrt(5 * HallThruster.e * 30 / (3 * HallThruster.Xenon.m))
+    tan_δ = 0.5 * max(0.0, min(π/4, cs / 20000))
+    A = π * ((r1 + tan_δ * 0.025)^2 - (max(0.0,r0 - tan_δ * 0.025))^2)
+
+    cache = (; channel_area = zeros(4), inner_radius = zeros(4), 
+            outer_radius = zeros(4), channel_height = zeros(4), 
+            dA_dz = zeros(4), tanδ = zeros(4), Tev = [30])
+    config = (; thruster = SPT_100, ncharge = 1, solve_plume = true)
+    index = (; ρi = [1], ρiui = [2])
+    params = (; mi = HallThruster.Xenon.m, z_cell = [0.0, 0.01, 0.025, 0.05],
+                L_ch = 0.025, exit_plane_index = 1, config = config, A_ch = A_ch, 
+                cache = cache, index = index )
+
+    HallThruster.update_plume_geometry!(U, params)
+
+    @test params.cache.dA_dz[1] ≈ 0.0
+    @test params.cache.dA_dz[2] ≈ 0.0
+    @test params.cache.dA_dz[3] ≈ 0.0
+    @test params.cache.dA_dz[4] ≈ (A - A_ch) / 0.025
 end
