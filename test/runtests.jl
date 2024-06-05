3faf4cb3eb2c699238a5838937f332a202046ec9
Thomas Marks <marksta@umich.edu>
2024-06-05 13:31:19 -0400

    update OVS tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -51,12 +51,13 @@ end
 
 include("order_verification/ovs_ions.jl")
 @testset "Order verification (neutrals and ions)" begin
-    refinements = refines(6, 10, 2)
+    refinements = refines(5, 10, 2)
 
     limiter = HallThruster.van_leer
     flux_names = ["HLLE", "Rusanov", "Global Lax-Friedrichs"]
     #flux_names = ["Global Lax-Friedrichs"]
     fluxes = [HallThruster.HLLE, HallThruster.rusanov, HallThruster.global_lax_friedrichs,]
+    #fluxes = [HallThruster.HLLE, HallThruster.rusanov, HallThruster.global_lax_friedrichs,]
 
     # Which L-P norms to check
     cases = ["ρn", "ρi", "ρiui"]
@@ -65,13 +66,9 @@ include("order_verification/ovs_ions.jl")
 
     for (flux, flux_name) in zip(fluxes, flux_names)
         for reconstruct in [false, true]
-            if reconstruct && flux_name == "HLLE"
-                continue
-            end
             scheme = HallThruster.HyperbolicScheme(flux, limiter, reconstruct)
             orders, norms = test_refinements(ncells -> OVS_Ions.solve_ions(ncells, scheme), refinements, norms_to_test)
             for (i, (order, norm)) in enumerate(zip(orders, norms))
-
                 norm_ind = mod1(i, num_norms)
                 case_ind = ((i-1) ÷ num_norms) + 1
                 case_str = "($(cases[case_ind]), $(norms_to_test[norm_ind])-norm)"
@@ -84,9 +81,9 @@ include("order_verification/ovs_ions.jl")
 
                 # Check that we achieve the desired order of accuracy
                 if (reconstruct)
-                    @test order > 1.5
+                    @test order >= 1.5
                 else
-                    @test order > 0.85
+                    @test order > 0.75
                 end
             end
         end
