0d9f5e78f1c7b6f908c9797b2fcd7bfb4d674a4e
Thomas Marks <marksta@umich.edu>
2024-08-07 16:10:03 -0600

    Fix plot recipes

--- src/visualization/recipes.jl
+++ src/visualization/recipes.jl
@@ -1,6 +1,6 @@
 @recipe function f(sol::Solution, frame::Int = length(sol.u))
     (;z_cell, ionization_reactions, ncharge, L_ch) = sol.params
-    model = params.config.ionization_model
+    model = sol.params.config.ionization_model
 
     subplot_width = 500
     subplot_height = 500
@@ -106,7 +106,7 @@
                     else
                         reactant_density = sol[:ni, rxn.reactant.Z][frame]
                     end
-                    ionization_rate .+= reactant_density .* ne .* rate_coeff.(model, rxn, 3/2 .* Tev)
+                    ionization_rate .+= reactant_density .* ne .* [rate_coeff(model, rxn, 3/2 .* T) for T in Tev]
                 end
             end
         end
