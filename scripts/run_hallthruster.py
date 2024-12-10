2aea72cbd6d971da4e2c722e483c2225b6f39f6c
Thomas Marks <marksta@umich.edu>
2024-12-09 20:43:58 -0700

    Add script to run HallThruster.jl

--- scripts/run_hallthruster.py
+++ scripts/run_hallthruster.py
@@ -3,67 +3,92 @@ import os
 from pathlib import Path
 import subprocess
 import tempfile
-from typing import Any
 
 def run_hallthruster_jl(
-    input: dict[str, Any] | str | Path,
-    jl_environment: str = ""
-    ):
-    """Run a single HallThruster.jl simulation for a given set of inputs
+        input: dict | str | Path,
+        jl_env: str | Path | None = None,
+        jl_script: str | Path | None = None,
+        **kwargs) -> dict:
+    """Python wrapper for `HallThruster.run_simulation(json_input)` in Julia.
 
     :param json_input: either a dictionary containing `config`, `simulation`, and `postprocess` options for 
             HallThruster.jl, or a string/Path containing a path to a JSON file with those inputs.
-            See the HallThruster.jl documentation for the required keys.
-    :param jl_environment: The julia environment containing HallThruster.jl. Defaults to global Julia environment.
+    :param jl_env: The julia environment containing HallThruster.jl. Defaults to global Julia environment.
+    :param jl_script: path to a custom Julia script to run. The script should accept the input json file path as
+                      a command line argument. Defaults to just calling `HallThruster.run_simulation(input_file)`.
+    :param kwargs: additional keyword arguments to pass to `subprocess.run` when calling the Julia script.
 
-    :returns: `dict` of `Hallthruster.jl` outputs: `I_B0`, `I_d`, `T`, `eta_c`, `eta_m`, `eta_v`, and `u_ion` for ion
-              beam current (A), discharge current (A), thrust (N), current efficiency, mass efficiency, voltage
-              efficiency, and singly-charged ion velocity profile (m/s). The specific outputs depend on the settings
-              provided in the `postprocess` dict in the input.
-              If `postprocess['output_file']` is present, this function will also write the requested outputs and
-              restart information to that file.
+    :returns: `dict` of `Hallthruster.jl` outputs. The specific outputs depend on the settings
+              provided in the `postprocess` dict in the input. If `postprocess['output_file']` is present,
+              this function will also write the requested outputs and restart information to that file.
     """
-
     # Read JSON input from file if path provided
-    if isinstance(input, str) or isinstance(input, Path):
-        with open(input) as fp:
-            json_input : dict[str, Any] = json.load(fp)
+    if isinstance(input, str | Path):
+        with open(input, 'r') as fp:
+            json_input = json.load(fp)
     else:
         json_input = input
 
+    tempfile_args = dict(suffix=".json", prefix="hallthruster_jl_", mode="w", delete=False, encoding="utf-8")
+
     # Get output file path. If one not provided, create a temporary
-    if 'output_file' in json_input['postprocess']:
+    temp_out = False
+    if 'output_file' in json_input.get('postprocess', {}):
         output_file = Path(json_input['postprocess'].get('output_file'))
+    elif 'output_file' in json_input.get('input', {}).get('postprocess', {}):
+        output_file = Path(json_input['input']['postprocess'].get('output_file'))
     else:
-        # Create temporary
-        fd_out = tempfile.NamedTemporaryFile(suffix=".json", prefix="hallthrusterjl_")
+        temp_out = True
+        fd_out = tempfile.NamedTemporaryFile(**tempfile_args)
         output_file = Path(fd_out.name)
         fd_out.close()
-        json_input['postprocess']['output_file'] = str(output_file)
+
+        if json_input.get('input'):
+            json_input['input'].setdefault('postprocess', {})
+            json_input['input']['postprocess']['output_file'] = str(output_file.resolve())
+        else:
+            json_input.setdefault('postprocess', {})
+            json_input['postprocess']['output_file'] = str(output_file.resolve())
 
     # Dump input to temporary file
-    fd = tempfile.NamedTemporaryFile(
-        suffix=".json", prefix="hallthrusterjl_",
-        mode="w", delete=False, encoding="utf-8"
-    )
+    fd = tempfile.NamedTemporaryFile(**tempfile_args)
     input_file = fd.name
     json.dump(json_input, fd, ensure_ascii=False, indent=4)
     fd.close()
 
     # Run HallThruster.jl on input file
+    if jl_script is None:
+        cmd = ['julia', '--startup-file=no', '-e',
+               f'using HallThruster; HallThruster.run_simulation(raw"{input_file}")']
+    else:
+        cmd = ['julia', '--startup-file=no', '--',
+               str(Path(jl_script).resolve()), input_file]
+
+    if jl_env is not None:
+        if Path(jl_env).exists():
+            cmd.insert(1, f'--project={Path(jl_env).resolve()}')
+        else:
+            raise ValueError(f"Could not find Julia environment {jl_env}. Please create it first. "
+                             f"See https://github.com/JANUS-Institute/HallThrusterPEM/blob/main/scripts/install_hallthruster.py")
+
     try:
-        subprocess.run([
-            'julia',
-            f'--project=${jl_environment}',
-            '-e'
-            f'using HallThruster; HallThruster.run_simulation("{input_file}")'
-        ])
+        subprocess.run(cmd, **kwargs)
     finally:
         # Delete temporary input file
         os.unlink(input_file)
 
     # Load output data
-    with open(output_file) as fp:
+    with open(output_file, 'r') as fp:
         output_data = json.load(fp)
+
+    if temp_out:
+        os.unlink(output_file)
+        if d := output_data.get('postprocess'):
+            if 'output_file' in d:
+                del d['output_file']
+        if d := output_data.get('input'):
+            if d2 := d.get('postprocess'):
+                if 'output_file' in d2:
+                    del d2['output_file']
 
     return output_data
