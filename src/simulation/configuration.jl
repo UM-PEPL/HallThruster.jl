78de8a07f60dde4d46bfb8778a1e6dc3f47d9b86
Karol Lesnick <klesnick@umich.edu>
2026-07-23 13:36:32 -0400

    fixed excited state tracking

--- src/simulation/configuration.jl
+++ src/simulation/configuration.jl
@@ -325,9 +325,10 @@ function load_propellant_config(propellant_config; directories = String[], verbo
         ion_temperature_K = get(gas_dict, "ion_temperature_K", nothing)
         allowed_charges = get(gas_dict, "allowed_charges", nothing)
         max_charge = get(gas_dict, "max_charge", nothing)
+        excited_levels = get(gas_dict, "excited_levels", nothing)
         flow_rate_kg_s = get(gas_dict, "flow_rate_kg_s", 0.0)
 
-        push!(props, Propellant(; gas, allowed_charges, max_charge, flow_rate_kg_s, temperature_K, velocity_m_s, ion_temperature_K))
+        push!(props, Propellant(; gas, allowed_charges, max_charge, excited_levels, flow_rate_kg_s, temperature_K, velocity_m_s, ion_temperature_K))
     end
 
     return props
